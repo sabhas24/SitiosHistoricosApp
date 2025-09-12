586bcb58c23c2555873d1da545d5e65f99a457f7
Ivana Merino <ivi7793@gmail.com>
2025-09-12 13:28:55 -0300

    Se agrega hoja de estilos y manejador de errores

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,10 +1,25 @@
 from flask import Flask
 from flask import render_template
+from src.web.handlers import error
+from flask import abort
 
-def create_app(env="development",static_folder=""):
-    app = Flask(__name__)
+def create_app(env="development", static_folder="../../static"):
+    app = Flask(__name__, static_folder=static_folder)
 
     @app.route('/')
     def home():
         return render_template('home.html')
+    
+    @app.route('/error401')
+    def prueba_401():
+        abort(401)  
+
+    @app.route('/error500')
+    def prueba_500():
+        return 1 / 0 
+    
+    app.register_error_handler(404, error.not_found)
+    app.register_error_handler(401, error.unauthorized)
+    app.register_error_handler(500, error.internal_error)
+
     return app
