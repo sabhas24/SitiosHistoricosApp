644dac70d3ad07ee4a4e688d0340966a48285a86
Pablo Redruello <pablo.redruello246513@alumnos.info.unlp.edu.ar>
2025-09-24 20:45:23 -0300

    hashing pasword

--- admin/src/models/auth/__init__.py
+++ admin/src/models/auth/__init__.py
@@ -2,15 +2,24 @@ from src.models.database import db
 from src.models.auth.user import user
 from src.models.auth.role import Role
 from src.models.auth.permission import Permission
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def user_new(**kwargs):
     print(" 📝Creating a new user with the following details:")
+    if 'password' in kwargs:
+        kwargs['password'] = generate_password_hash(kwargs['password'])
     new= user(**kwargs)
     db.session.add(new)
     db.session.commit()
     print(f" ✅ User created with ID: {new.id}")
     return new
-
+def user_check_password(username, password):
+    user_obj=db.session.query(user).filter_by(username=username).first()
+    if user_obj and check_password_hash(user_obj.password, password):
+        print(f" ✅ Password for user '{username}' is correct.")
+        return True
+    print(f" ❌ Password for user '{username}' is incorrect or user does not exist.")
+    return False
 def user_index():
     return db.session.query(user).all()
 
@@ -57,8 +66,8 @@ def create_permission(name):
 
 def assign_permission_to_role(role_name, perm_name):
     print(f" 📝Assigning permission '{perm_name}' to role '{role_name}'")
-    role = db.session.query(Role).filter_by(name=role_name).first()  # Cambia Role.query a db.session.query(Role)
-    perm = db.session.query(Permission).filter_by(name=perm_name).first()  # Cambia Permission.query a db.session.query(Permission)
+    role = db.session.query(Role).filter_by(name=role_name).first()  
+    perm = db.session.query(Permission).filter_by(name=perm_name).first() 
     if role and perm and perm not in role.permissions:
         role.permissions.append(perm)
         db.session.commit()
