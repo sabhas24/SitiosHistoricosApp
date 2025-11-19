8011e26973123299571116fd1ee6bdfb59a09115
justoalvarado1470 <justoalvarado1470@gmail.com>
2025-11-19 17:53:36 -0300

    config api front

--- portal/src/config/api.js
+++ portal/src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default configuration
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+    baseURL: 'https://grupo44.proyecto2025.linti.unlp.edu.ar/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
